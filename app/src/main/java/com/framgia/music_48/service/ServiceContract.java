170852e7bafde50bc12f8577a28bd228729d7256
LUC <vovanluc.3108@gmail.com>
2019-01-18 11:15:31 +0700

    task 5150 play pause next previous music

---
+++ app/src/main/java/com/framgia/music_48/service/ServiceContract.java
@@ -0,0 +1,17 @@
+package com.framgia.music_48.service;
+
+import com.framgia.music_48.data.model.Song;
+
+public interface ServiceContract {
+    interface onMediaPlayer {
+        void onUpdateUIListener(Song song);
+
+        void onUpdateButtonStateListener(boolean isPlaying);
+
+        void onUpdateSeekBarListener();
+
+        void onLoopStateListener(int loopState);
+
+        void onShuffleStateListener(boolean isShuffleState);
+    }
+}
